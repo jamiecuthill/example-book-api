14ee71d58df3873b93e3a2f3be4b79a25123514f
Jamie Cuthill <jamiecuthill@flypay.co.uk>
2019-09-04 11:47:43 +0100

    book title code

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
+	"path"
 	"syscall"
 	"time"
 
@@ -15,43 +18,28 @@ import (
 
 func main() {
 
+	openlibrary, err := url.Parse("https://openlibrary.org")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	r := mux.NewRouter()
 
-	//
-	// {"type": "foo", "data": {"name": "foo"}}
-	//
-
-	r.Methods(http.MethodPost).
-		Path("/req").
-		HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			var body struct {
-				Type string
-				Data json.RawMessage
-			}
-			_ = json.NewDecoder(r.Body).Decode(&body)
-
-			// var subtype struct{}
-			// json.Unmarshal(body.Data, &subtype)
-
-			// if body["hello"] == "" {
-			// 	w.WriteHeader(http.StatusBadRequest)
-			// 	return
-			// }
-
-			w.WriteHeader(http.StatusOK)
-
-			fmt.Fprintf(w, "msg is %+v", body)
-		})
+	r.Methods(http.MethodGet).
+		Path("/books/{isbn}").
+		HandlerFunc(handleBookReq(&bookrepo{
+			server: openlibrary,
+		}))
 
 	server := &http.Server{
 		Addr:    ":8080",
-		Handler: withAuth(r),
+		Handler: r,
 	}
 
 	go func() {
 		fmt.Println("Running http server")
 		err := server.ListenAndServe()
-		fmt.Println(err)
+		fmt.Printf("server error: %+v\n", err)
 	}()
 
 	c := make(chan os.Signal, 1)
@@ -59,20 +47,62 @@ func main() {
 
 	inter := <-c
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	server.Shutdown(ctx)
 
-	fmt.Printf("Finished %s", inter)
+	fmt.Printf("Finished %s\n", inter)
 }
 
-func withAuth(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("Authorization") == "" {
-			w.WriteHeader(http.StatusUnauthorized)
+func handleBookReq(repo *bookrepo) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		isbn := mux.Vars(r)["isbn"]
+
+		name, err := repo.getBookName(isbn)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Error: %+v\n", err)
 			return
 		}
 
-		next.ServeHTTP(w, r)
-	})
+		fmt.Fprintf(w, "Your book is: %s\n", name)
+	}
+}
+
+type bookrepo struct {
+	server *url.URL
+}
+
+func (r *bookrepo) getBookName(isbn string) (string, error) {
+	// https://openlibrary.org/api/books?bibkeys=ISBN:9780134190440&format=json
+
+	bookUrl := *r.server
+	bookUrl.Path = path.Join(r.server.Path, "/api/books")
+	q := bookUrl.Query()
+	q.Set("format", "json")
+	q.Set("bibkeys", "ISBN:"+isbn)
+	q.Set("jscmd", "data")
+	bookUrl.RawQuery = q.Encode()
+
+	req, err := http.NewRequest(http.MethodGet, bookUrl.String(), nil)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("Unexpected status code from openlibrary: %d", resp.StatusCode)
+	}
+
+	var respData map[string]interface{}
+	_ = json.NewDecoder(resp.Body).Decode(&respData)
+
+	name := respData["ISBN:"+isbn].(map[string]interface{})["title"].(string)
+
+	return name, nil
 }
