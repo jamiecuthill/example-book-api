2c2ab78d4e9887f7433bf842abcc457a4f03ed40
Jamie Cuthill <jamiecuthill@flypay.co.uk>
2019-09-04 14:29:05 +0100

    Write test for happy paths of handler and book name getter

--- library_test.go
+++ library_test.go
@@ -1,20 +1,41 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"net/url"
 	"testing"
 
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestReturnsBookNameForISBN(t *testing.T) {
+	requests := make(chan *http.Request)
+	server := httptest.NewServer(mockOpenLibrary(requests))
+	defer server.Close()
+
+	serverURL, _ := url.Parse(server.URL)
+
 	library := &bookrepo{
-		server: &url.URL{},
+		server: serverURL,
 	}
 
 	name, err := library.GetBookName("9780134190440")
 	assert.NoError(t, err)
 	assert.Equal(t, "The Go programming language", name)
+
+	r := <-requests
+	assert.Equal(t, "/api/books", r.URL.Path)
+}
+
+func mockOpenLibrary(requests chan *http.Request) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		go func() {
+			requests <- r
+		}()
+		w.Write([]byte(fixtureISBNResponse))
+	})
 }
 
 var fixtureISBNResponse = `{
@@ -72,3 +93,24 @@ var fixtureISBNResponse = `{
 	   }
 	}
  }`
+
+func TestHandleBookReqRequestsStatusOK(t *testing.T) {
+	requests := make(chan *http.Request)
+	server := httptest.NewServer(mockOpenLibrary(requests))
+	defer server.Close()
+
+	serverURL, _ := url.Parse(server.URL)
+
+	library := &bookrepo{
+		server: serverURL,
+	}
+
+	rw := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodGet, "/books/9780134190440", nil)
+	r = mux.SetURLVars(r, map[string]string{"isbn": "9780134190440"})
+
+	handleBookReq(library).ServeHTTP(rw, r)
+
+	assert.Equal(t, http.StatusOK, rw.Code)
+	assert.Equal(t, "Your book is: The Go programming language\n", rw.Body.String())
+}
