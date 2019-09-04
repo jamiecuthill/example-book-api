f628bfb4409bd716a9c1ea695d3db9c99848986d
Jamie Cuthill <jamiecuthill@flypay.co.uk>
2019-09-04 11:52:57 +0100

    Separate for testing

---
+++ library.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"path"
+)
+
+type bookrepo struct {
+	server *url.URL
+}
+
+func (r *bookrepo) getBookName(isbn string) (string, error) {
+	// https://openlibrary.org/api/books?bibkeys=ISBN:9780134190440&format=json
+
+	bookUrl := *r.server
+	bookUrl.Path = path.Join(r.server.Path, "/api/books")
+	q := bookUrl.Query()
+	q.Set("format", "json")
+	q.Set("bibkeys", "ISBN:"+isbn)
+	q.Set("jscmd", "data")
+	bookUrl.RawQuery = q.Encode()
+
+	req, err := http.NewRequest(http.MethodGet, bookUrl.String(), nil)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("Unexpected status code from openlibrary: %d", resp.StatusCode)
+	}
+
+	var respData map[string]interface{}
+	_ = json.NewDecoder(resp.Body).Decode(&respData)
+
+	name := respData["ISBN:"+isbn].(map[string]interface{})["title"].(string)
+
+	return name, nil
+}
--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"os/signal"
-	"path"
 	"syscall"
 	"time"
 
@@ -67,42 +65,4 @@ func handleBookReq(repo *bookrepo) http.HandlerFunc {
 
 		fmt.Fprintf(w, "Your book is: %s\n", name)
 	}
-}
-
-type bookrepo struct {
-	server *url.URL
-}
-
-func (r *bookrepo) getBookName(isbn string) (string, error) {
-	// https://openlibrary.org/api/books?bibkeys=ISBN:9780134190440&format=json
-
-	bookUrl := *r.server
-	bookUrl.Path = path.Join(r.server.Path, "/api/books")
-	q := bookUrl.Query()
-	q.Set("format", "json")
-	q.Set("bibkeys", "ISBN:"+isbn)
-	q.Set("jscmd", "data")
-	bookUrl.RawQuery = q.Encode()
-
-	req, err := http.NewRequest(http.MethodGet, bookUrl.String(), nil)
-	if err != nil {
-		return "", err
-	}
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("Unexpected status code from openlibrary: %d", resp.StatusCode)
-	}
-
-	var respData map[string]interface{}
-	_ = json.NewDecoder(resp.Body).Decode(&respData)
-
-	name := respData["ISBN:"+isbn].(map[string]interface{})["title"].(string)
-
-	return name, nil
 }
